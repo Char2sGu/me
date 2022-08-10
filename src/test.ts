523c8e197d886299c7b57abfd9176b91e7d21b76
Char2s <Char2s@outlook.com>
2022-08-10 10:31:37 +0800

    run linter

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,19 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
+
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp,
+  ): {
     <T>(id: string): T;
     keys(): string[];
   };
@@ -21,6 +26,6 @@ getTestBed().initTestEnvironment(
 );
 
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/u);
 // And load the modules.
 context.keys().forEach(context);
