0a3d7942e4d7d09cf99e4a76d015db9a1046b595
Char2s <Char2s@outlook.com>
2022-08-13 17:34:13 +0800

    feat: implement header

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeHeaderComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })
