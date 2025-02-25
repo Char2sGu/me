b1ba1cbcf9c6130df3a13a099e7b34595d053469
Charles <char2s@outlook.com>
2025-02-24 22:19:53 -0500

    ng new

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'crs-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
